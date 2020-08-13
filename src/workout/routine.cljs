ebeac489eef49f34f59711a76f38a898a0510980
Canna Wen <cannawen@gmail.com>
2020-08-13 16:49:42 -0400

    Refactor logic around choosing exercises for routine

--- src/workout/routine.cljs
+++ src/workout/routine.cljs
@@ -7,16 +7,19 @@
 (defn make-routine [exercise-count]
   (->> (range exercise-count)
        (reduce (fn [memo _]
-                 (let [previous-exercise (last (memo :routine))
-                       next-exercise (->> exercises
-                                          (filter (fn [exercise]
-                                                    (empty? (set/intersection (exercise :tags)
-                                                              (or (:tags previous-exercise)
-                                                                  #{})))))
-                                          (shuffle)
-                                          (first))]
-                   (-> memo
-                       (update :available-exercises disj next-exercise)
-                       (update :routine conj next-exercise))))
+                 (if (empty? (memo :available-exercises))
+                   (reduced memo)
+                   (let [previous-exercise-tags (or (:tags (last (memo :routine))) #{})
+                         next-exercise (or (->> (memo :available-exercises)
+                                                (filter (fn [exercise]
+                                                          (empty? (set/intersection
+                                                                    (exercise :tags)
+                                                                    previous-exercise-tags))))
+                                                (shuffle)
+                                                (first))
+                                         (rand-nth (vec (memo :available-exercises))))]
+                     (-> memo
+                         (update :available-exercises disj next-exercise)
+                         (update :routine conj next-exercise)))))
          {:available-exercises exercises :routine []})
        :routine))
