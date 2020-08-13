d7c03d42564a938bfe317b4686d7d261f0b4e146
Canna Wen <cannawen@gmail.com>
2020-08-13 18:08:13 -0400

    Speak with UK female voice

--- src/workout/speech.cljs
+++ src/workout/speech.cljs
@@ -1,4 +1,11 @@
 (ns workout.speech)
 
+(def voice
+  (.. js/window.speechSynthesis
+    (getVoices)
+    (find (fn [v]
+            (= (.-voiceURI v) "Google UK English Female")))))
+
 (defn speak! [text]
-  (.. js/window.speechSynthesis (speak (js/SpeechSynthesisUtterance. text))))
+  (.. js/window.speechSynthesis (speak (doto (js/SpeechSynthesisUtterance. text)
+                                             (aset "voice" voice)))))
