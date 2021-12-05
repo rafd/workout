917d5cdab183b76aff36707eb6e83bdb009a0ac3
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-12-05 17:09:28 -0500

    Delay setting of voice (to handle browser hijinks)

--- src/workout/speech.cljs
+++ src/workout/speech.cljs
@@ -1,17 +1,23 @@
 (ns workout.speech)
 
-(def voice
-  (.. js/window.speechSynthesis
-      (getVoices)
-      (find (fn [v]
-              (= (.-voiceURI v) "Google UK English Female")))))
+(def voice (atom nil))
 
+(defonce _set_voice_
+  (.addEventListener js/speechSynthesis
+                     "voiceschanged"
+                     (fn [_]
+                       (reset! voice (.. js/window.speechSynthesis
+                                         (getVoices)
+                                         (find (fn [v]
+                                                 (= (.-voiceURI v) "Google UK English Female"))))))))
 (defn speak!
   ([text]
    (speak! text (fn [])))
 
   ([text callback]
-   (.. js/window.speechSynthesis (speak (doto (js/SpeechSynthesisUtterance. text)
-                                              (aset "voice" voice)
-                                              (.addEventListener "end" callback))))))
+   (if text
+    (.. js/window.speechSynthesis (speak (doto (js/SpeechSynthesisUtterance. text)
+                                               (aset "voice" @voice)
+                                               (.addEventListener "end" callback))))
+    (callback))))
 
